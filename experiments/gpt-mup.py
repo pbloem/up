afe768301add2dad27bf2f22cde5b8c05976720a
Peter Bloem <git@peterbloem.nl>
2024-07-17 00:59:15 +0200

    ...

--- experiments/gpt-mup.py
+++ experiments/gpt-mup.py
@@ -74,7 +74,8 @@ def go(
          heads : int,                 # Number of heads. The rest of the model parameters are derived from this.
          context=512,
          temperature=0.5,
-         target_microbatch_size=100,  # microbatch size for the target model
+         target_microbatch_size=52,  # microbatch size for the target model
+         macrobatch_size=256,         # macrobatch size for the target model
          source_batch_mult=2.0,       # how much bigger the batch size is for the source model than the microbatch size
                                       # for the target model
          buffer_size_mult=20,         # How much bigger the buffer is than one source batch
@@ -88,7 +89,6 @@ def go(
          print_every=500,             # How often to print the source output
          gc=1.0,                      # Gradient clipping.
          eval_samples=10_000,         # On how many samples to evaluate
-         mlm_prob=0.15,
          init_mult_max=50.0,          # multiplier for the weights of the source model
          mask_prob_max=0.7,
          nonlinearity='relu',
@@ -96,18 +96,17 @@ def go(
          eval_ood=False,              # Whether to evaluate on OOD datasets
          name=None,                   # WandB name
          eval_batch_mult=2.0,         # How much bigger the eval batches can be than the training batches
-         accumulate = 1,              # The number of batches to accumulate the gradient over before a gradient step occurs
          cp_every = 100_000,          # Save a checkpoint for the model every n batches.
          dp = False,                  # Use data-parallel (multi GPU)
-         acc_warmup = 0               # Accumulation warmup (in instances)
+         mbwarmup = 100_000                # Accumulation warmup (in instances)
        ):
 
     """
-    Scaling experiment. Uses Levine 2021 to det the depth/width and Yang 2022
+    Scaling experiment. Uses Levine 2021 to get the depth/width and Yang 2022 to scale the lr and initialization.
 
     We first scale the width (d_model) by the number of heads. Note that the nr of heads itself doesn't matter very much,
     but it's a convenient way to scale the width. We hold to the convention that the number of dimensions in the width
-    per head should be constant (at 110 in our case).
+    per head should be constant (at 128 in our case).
 
     We then use the principles from Yang 2022 (muP parametrization) to scale the weight initialization and the learning
     rate.
@@ -117,8 +116,12 @@ def go(
 
     """
 
+    # Compute some values that should be logger to wandb
     width = heads * WIDTHPERHEAD
     depth = get_depth(width)
+
+    source_microbatch_size = int(round(target_microbatch_size * source_batch_mult))
+    buffer_size = int(round(source_microbatch_size * buffer_size_mult))
 
     wd = wandb.init(
         name=name,
@@ -148,7 +151,6 @@ def go(
     # Target for training
     model = up.GTransformer(emb=width, heads=heads, depth=depth, seq_length=context, num_tokens=NUM_TOKENS, nosqrt=True)
 
-
     if torch.cuda.is_available():
         model.cuda()
     if dp:
@@ -158,263 +160,162 @@ def go(
 
     print(opt)
 
-    exit()
-
     if warmup > 0:
         # warmup = warmup / accumulate
         # sch = torch.optim.lr_scheduler.LambdaLR(opt, lambda i: min(i / (warmup / model_batch_size), 1.0))
+        for g in opt.param_groups:
+            g['max_lr'] = g['lr']
+            g['lr_delta'] = g['lr'] / warmup
 
-        maxlr = lr
-        lr = 0.0
-        lrdelta = maxlr / warmup
+            g['lr'] = 0.0
 
+    cmp_source = up.GTransformer(emb=width, heads=heads, depth=depth, seq_length=context, num_tokens=NUM_TOKENS, nl=nl(nonlinearity), mask_channel=True)
+
+    if torch.cuda.is_available():
+        cmp_source.cuda()
+
+    if dp:
+        cmp_source = torch.nn.DataParallel(cmp_source)
+
+    buffer = torch.randint(low=0, high=NUM_TOKENS, size=(buffer_size, context), device=d())
+
+    sampletime = -1.0
+
+    print('Start pre-training')
+
+    accumulated = 0 # nr of instances accumulated currently
+    if mbwarmup > 0:
+        mbraw = target_microbatch_size # macrobatch size as a float
+        mbdelta = (macrobatch_size - target_microbatch_size) / mbwarmup
     else:
-        if mult_lr:
-            lr = lr * accumulate
+        mbraw = macrobatch_size
 
-    # Load a pretrained model
-    if model_file is not None:
-        checkpoint = torch.load(model_file)
+    for i in (bar := trange(batches)):
 
-        model.load_state_dict(checkpoint['model_state_dict'])
-        opt.load_state_dict(checkpoint['optimizer_state_dict'])
-
-    # Pretrain a model
-    else:
-
-        if pre_file is None:
-            cmp_source = up.GTransformer(emb=cemb, heads=cheads, depth=cdepth, seq_length=context, num_tokens=NUM_TOKENS, nl=nl(nonlinearity), mask_channel=True)
-
-            if torch.cuda.is_available():
-                cmp_source.cuda()
-
-            if dp:
-                cmp_source = torch.nn.DataParallel(cmp_source)
-
-            buffer = torch.randint(low=0, high=NUM_TOKENS, size=(buffer_size, context), device=d())
-
-        else:
-            with gzip.open(pre_file, 'r') as file:
-                buffer = file.read()
-                buffer = torch.tensor([int(byte) for byte in buffer], dtype=torch.long)
-                buffer = buffer.reshape(-1, context)
-
-                buffer_size = buffer.size(0)
-
-        sampletime = -1.0
-
-        # Pretraining batches
-        if pre_batches > 0:
-
-            print('Start pre-training')
-
-            accumulated = acc_last = 0
-            if acc_warmup > 0:
-                accraw = 1.0
-                accdelta = (accumulate - 1) / acc_warmup
-            else:
-                accraw = accumulate
-                accdelta = 0.0
-
-            for i in (bar := trange(pre_batches)):
-
-                # if cp_every > 0 and i > 0 and i % cp_every == 0:
-                #
-                #     if model_dst is not None:
-                #         print(f'Saving model at {i * model_batch_size}.')
-                #         torch.save({
-                #             'model_state_dict': model.state_dict(),
-                #             'optimizer_state_dict': opt.state_dict(),
-                #         }, model_dst.format(i * model_batch_size))
-
-                if eval_every > 0 and i % eval_every == 0 and not skip_eval:
-
-                    for name, data in datasets.items():
-                        print(f'evaluating {name}')
-
-                        with torch.no_grad():
-                            est = estimate_compression(
-                                model=model,
-                                data=data,
-                                nsamples=eval_samples,
-                                context=context,
-                                batch_size=int(model_batch_size * eval_batch_mult),
-                                model_produces_logits=True
-                            )
-
-                        wandb.log({f'val-{name}': est})
-
-                if pre_file is None: # Sample on the fly.
-
-                    tic()
-                    with torch.no_grad():
-                        # Re-initialize the parameters of source (i.e. sample a random source)
-                        up.weights_init(cmp_source, init_mult_max=init_mult_max, mask_prob_max=mask_prob_max)
-
-                        # slice a random selection of rows from the buffer (without replacement)
-                        iz = random.sample(range(buffer.size(0)), sample_batch_size)
-                        z = buffer[iz, :]
-
-                        # replace some random rows with uniform random characters
-                        rows = torch.bernoulli(torch.full(size=(sample_batch_size, 1), fill_value=reset_prob))
-                        mask = rows.expand(sample_batch_size, context).to(torch.bool)
-
-                        uniform = torch.randint(low=0, high=NUM_TOKENS, size=(sample_batch_size, context), device=d())
-                        z[mask] = uniform[mask]
-
-                        # pass it through a randomly chosen model
-                        output = cmp_source(z)
-
-                        # The model generates an output sequence as well as a mask. The final sequence has the output
-                        # characters at the masked positions and the input characters at the non-masked positions. The
-                        # idea is that this makes it more likely for the model to retain any structure present in the
-                        # input.
-                        # -- It is theoretically possible for the model to do this without the masking, but it's very
-                        #    unlikely with random parameters.
-                        chars, mask = output[:, :, :-1], output[:, :, -1]
-
-                        chars = sample(chars, temperature=temperature)
-                        mask = torch.sigmoid(mask).to(torch.bool)
-
-                        z[mask] = chars[mask]
-
-                        buffer[iz, :] = z
-
-                    sampletime = toc()
-
-                tic()
-                # Perform a training step on batches sampled from the buffer
-
-                iz = random.sample(range(buffer_size), model_batch_size)
-
-                batch = buffer[iz, :]
-                if torch.cuda.is_available():
-                    batch = batch.cuda()
-
-                input  = batch[:, :-1]
-                target = batch[:, 1:]
-
-                with torch.cuda.amp.autocast():
-                    output = model(input)
-                    rloss = F.cross_entropy(output.transpose(2, 1), target)
-                    loss = rloss / accumulate # scale the loss to compensate for the accumulation
-
-                scaler.scale(loss).backward()
-                accumulated += 1
-
-                acc_current = min(int(round(accraw)), accumulate)
-                if i > acc_last + acc_current: # perform a step
-
-                    gn = gradient_norm(model)
-
-                    if gc > 0.0:
-                        nn.utils.clip_grad_norm_(model.parameters(), gc)
-
-                    wandb.log({
-                        'gradient_norm': gn,
-                        'accumulated': accumulated # Sanity check.
-                    })
-
-                    scaler.step(opt)
-                    scaler.update()
-
-                    opt.zero_grad()
-
-                    accumulated = 0
-                    acc_last = i
-
-                traintime = toc()
-
-                wandb.log({
-                    'loss': rloss.item(),
-                    'learning_rate': lr * acc_current if mult_lr else lr,
-                    'sample_time': sampletime,
-                    'train_time': traintime,
-                    'pre-training': 1.0,
-                    'accumulate': accraw
-                })
-                bar.set_postfix({'loss': f'{rloss.item():.02}'})
-
-                if acc_warmup > 0 and accraw < accumulate:
-                    accraw += accdelta * batch.size(0)
-
-                if warmup > 0:
-                    lr += lrdelta * batch.size(0)
-
-                    set_lr(lr * acc_current if mult_lr else lr, opt)
-
-                if i % print_every == 0:
-
-                    print('target')
-                    print_batch(batch[:4, :], ascii_only)
-
-                    print('model output')
-
-                    seed = torch.randint(low=0, high=NUM_TOKENS, size=(4, 1), device=d())
-                    output = sample_sequence(model, seed, context, num_tokens = NUM_TOKENS, length=context,
-                                                    temperature = temperature)
-                    print_batch(output, ascii_only)
-
-    # Fine-tuning
-    print('Start finetuning')
-
-    if warmup > 0:
-        sch = torch.optim.lr_scheduler.LambdaLR(opt, lambda i: min(i / (warmup / model_batch_size), 1.0))
-    # -- We keep the same optimizer (the statistics may well carry over from pre-training to finetuning)
-    #    but we reset the learning rate warmup.
-
-    traindata = torch.tensor(up.data.load_data('wp-train'), device='cpu')
-
-    for i in (bar := trange(num_batches)):
         if eval_every > 0 and i % eval_every == 0 and not skip_eval:
 
-            for name, valdata in datasets.items():
+            for name, data in datasets.items():
                 print(f'evaluating {name}')
 
                 with torch.no_grad():
                     est = estimate_compression(
                         model=model,
-                        data=valdata,
+                        data=data,
                         nsamples=eval_samples,
                         context=context,
-                        batch_size=int(model_batch_size * eval_batch_mult),
+                        batch_size=int(target_microbatch_size * eval_batch_mult),
                         model_produces_logits=True
                     )
 
                 wandb.log({f'val-{name}': est})
 
-        opt.zero_grad()
+        # Sample noise from a random model and insert into the buffer
+        tic()
+        with torch.no_grad():
+            # Re-initialize the parameters of source (i.e. sample a random source)
+            up.weights_init(cmp_source, init_mult_max=init_mult_max, mask_prob_max=mask_prob_max)
 
-        # sample a batch from the data
-        source, target = sample_batch(traindata, context, model_batch_size)
+            # slice a random selection of rows from the buffer (without replacement)
+            iz = random.sample(range(buffer.size(0)), source_microbatch_size)
+            z = buffer[iz, :]
 
+            # replace some random rows with uniform random characters
+            rows = torch.bernoulli(torch.full(size=(source_microbatch_size, 1), fill_value=reset_prob))
+            mask = rows.expand(source_microbatch_size, context).to(torch.bool)
+
+            uniform = torch.randint(low=0, high=NUM_TOKENS, size=(source_microbatch_size, context), device=d())
+            z[mask] = uniform[mask]
+
+            # pass it through a randomly chosen model
+            output = cmp_source(z)
+
+            # The model generates an output sequence as well as a mask. The final sequence has the output
+            # characters at the masked positions and the input characters at the non-masked positions. The
+            # idea is that this makes it more likely for the model to retain any structure present in the
+            # input.
+            # -- It is theoretically possible for the model to do this without the masking, but it's very
+            #    unlikely with random parameters.
+            chars, mask = output[:, :, :-1], output[:, :, -1]
+
+            chars = sample(chars, temperature=temperature)
+            mask = torch.sigmoid(mask).to(torch.bool)
+
+            z[mask] = chars[mask]
+
+            buffer[iz, :] = z
+
+        sampletime = toc()
+
+        tic()
+        # Perform a training step on batches sampled from the buffer
+
+        iz = random.sample(range(buffer_size), target_microbatch_size)
+
+        batch = buffer[iz, :]
         if torch.cuda.is_available():
-            source, target = source.cuda(), target.cuda()
+            batch = batch.cuda()
+
+        input  = batch[:, :-1]
+        target = batch[:, 1:]
 
         with torch.cuda.amp.autocast():
-            output = model(source)
-            loss = F.cross_entropy(output.transpose(2, 1), target)
+            output = model(input)
+            rloss = F.cross_entropy(output.transpose(2, 1), target)
+            loss = rloss
 
         scaler.scale(loss).backward()
+        accumulated += input.size(0)
 
-        gn = gradient_norm(model)
-        if gc > 0.0:
-            nn.utils.clip_grad_norm_(model.parameters(), gc)
+        if accumulated >= mbraw: # perform a step
 
-        scaler.step(opt)
-        scaler.update()
+            # scale the gradients to average over the macrobatch (so far, pytorch has summed the gradients)
+            for parm in model.parameters():
+                parm.grad /= accumulated
 
-        if warmup > 0:
-              sch.step()
+            gn = gradient_norm(model)
+
+            if gc > 0.0:
+                nn.utils.clip_grad_norm_(model.parameters(), gc)
+
+            wandb.log({
+                'gradient_norm': gn,
+                'accumulated': accumulated # Sanity check.
+            })
+
+            scaler.step(opt)
+            scaler.update()
+
+            opt.zero_grad()
+
+            accumulated = 0
+            acc_last = i
+
+        traintime = toc()
 
         wandb.log({
-            'loss': loss,
-            'learning_rate': sch.get_last_lr()[0],
-            'gradient_norm': gn,
-            'pre-training': 0.0
+            'loss': rloss.item(),
+            'learning_rate (stable)': opt.param_groups[0]['lr'],
+            'learning_rate (scaled)': opt.param_groups[1]['lr'],
+            'sample_time': sampletime,
+            'train_time': traintime,
+            'pre-training': 1.0,
+            'mbraw': mbraw
         })
 
-        bar.set_postfix({'loss': f'{loss:.02}'})
+        bar.set_postfix({'loss': f'{rloss.item():.02}'})
+
+        if mbwarmup > 0 and mbraw < macrobatch_size:
+            mbraw += mbdelta * batch.size(0)
+
+        if warmup > 0:
+            for g in opt.param_groups:
+                if g['lr'] < g['max_lr']:
+                    g['lr'] += g['lr_delta'] * batch.size(0)
+
+        if i % print_every == 0:
+
+            print('target samples', i)
+            print_batch(batch[:4, :], False)
 
 def throughput():
     """
